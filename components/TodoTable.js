5329077e4edadf438521079a5c7dbb85aaf7acd0
Thomas Nguyen <ttn@ou.edu>
2022-10-03 16:16:36 -0500

    Add task bar

--- components/TodoTable.js
+++ components/TodoTable.js
@@ -1,70 +1,109 @@
-import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Chip from '@mui/material/Chip';
-import { Checkbox } from '@mui/material';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography'
+import * as React from "react";
+import {
+  Checkbox,
+  TextField,
+  Chip,
+  TableRow,
+  TableHead,
+  TableContainer,
+  TableCell,
+  TableBody,
+  Table,
+  Paper,
+  Button,
+  Typography,
+} from "@mui/material";
+
+import dayjs from "dayjs";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 const rows = [
-    {
-        task: 'Buy some bread and milk',
-        module: 'A Label',
-        date: '9/18/1998',
-        id: 1
-    },
-    {
-        task: 'Buy some milk and bread',
-        module: 'A Label',
-        date: '11/03/2001',
-        id: 2
-    },
-    {
-        task: 'Buy some bread, and, milk',
-        module: 'A Label',
-        date: '11/17/2004',
-        id: 3
-    },
+  {
+    task: "Buy some bread and milk",
+    module: "A Label",
+    date: "9/18/1998",
+    id: 1,
+  },
+  {
+    task: "Buy some milk and bread",
+    module: "A Label",
+    date: "11/03/2001",
+    id: 2,
+  },
+  {
+    task: "Buy some bread, and, milk",
+    module: "A Label",
+    date: "11/17/2004",
+    id: 3,
+  },
 ];
 
 export default function TodoTable() {
-    return (
-        <TableContainer 
-        component = {Paper}
-        sx = {{maxWidth: '600px'}}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell align='left' colSpan={3}
-                        sx = {{
-                            borderBottom: 'none' 
-                            }}>
-                                <Typography variant="h6">To-do List</Typography></TableCell>
-                        <TableCell align='right' sx = {{borderBottom: 'none'}}><Button variant = "contained" color="secondary">Add New</Button></TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell padding='checkbox'/>
-                        <TableCell>Task</TableCell>
-                        <TableCell align='center'>Module</TableCell>
-                        <TableCell align='right'>Due Date</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow key = {row.id}>
-                            <TableCell padding='checkbox'><Checkbox color='success'></Checkbox></TableCell>
-                            <TableCell>{row.task}</TableCell>
-                            <TableCell align='center'><Chip label={row.module} color="info"/></TableCell>
-                            <TableCell align='right'>{row.date}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
+  const [value, setValue] = React.useState(dayjs("2014-08-18T21:11:54"));
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <TableContainer component={Paper} sx={{ maxWidth: "600px" }}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell
+              align="left"
+              colSpan={3}
+              sx={{
+                borderBottom: "none",
+              }}
+            >
+              <Typography variant="h6">To-do List</Typography>
+            </TableCell>
+            <TableCell align="right" sx={{ borderBottom: "none" }}>
+              <Button variant="contained" color="secondary">
+                Add New
+              </Button>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell padding="checkbox" />
+            <TableCell>
+              <TextField label="Task"></TextField>
+            </TableCell>
+            <TableCell align="center">
+              <TextField label="Module"></TextField>
+            </TableCell>
+            <TableCell align="right">
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  label="Date"
+                  value={value}
+                  onChange={(newValue) => {
+                    setValue(newValue);
+                  }}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell padding="checkbox">
+                <Checkbox color="success"></Checkbox>
+              </TableCell>
+              <TableCell>{row.task}</TableCell>
+              <TableCell align="center">
+                <Chip label={row.module} color="info" />
+              </TableCell>
+              <TableCell align="right">{row.date}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
