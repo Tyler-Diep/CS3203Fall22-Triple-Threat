8daf12c126687bf539ac2e2b504bac0cdbaea16a
Tiffany Le <tiffany.n.le-1@ou.edu>
2022-10-15 20:59:10 -0500

    Create a nav component

---
+++ components/NavBar.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import {
+    Button,
+    Stack,
+} from "@mui/material";
+import Link from "next/link";
+
+export default function TableNav() {
+
+    return (
+        <Stack direction="row" justifyContent="center" spacing={2} padding={1}>
+        <Link href="/" passHref><Button color="secondary">To-do List</Button></Link>
+        <Link href="/modules" passHref><Button color="secondary">Modules</Button></Link>
+        <Button color="secondary">Completed Tasks</Button>
+        <Link href="/deleted" passHref><Button color="secondary">Deleted Tasks</Button></Link>
+        </Stack>
+    )
+}
