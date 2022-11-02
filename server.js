6a39888cfdbdc2ddb84c876398c581240b7ea084
Tyler Diep <Tyler.T.Diep-1@ou.edu>
2022-11-02 15:56:41 -0500

    Added server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express();
+
+const uri = "mongodb+srv://TripleThreat:Tyl3r@softengfall2022-triplet.c1ph4p1.mongodb.net/?retryWrites=true&w=majority";
+
+async function connect() {
+    try {
+        await mongoose.connect(uri);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+connect();
+
+app.listen(8000, () => {console.log("Server started on port 8000");});
