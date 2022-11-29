c051e6d1f3840919c39d5b86d9d587e5cc058709
Tyler Diep <Tyler.T.Diep-1@ou.edu>
2022-11-29 17:33:08 -0600

    NextJS changes

---
+++ pages/api/users.js
@@ -0,0 +1,30 @@
+import server from '../../lib/server'
+import User from '../../models/model'
+
+export default async function handler (req, res) {
+  const { method } = req
+
+  await server()
+
+  switch (method) {
+    case 'GET':
+      try {
+        const users = await User.find({})
+        res.status(200).json({ success: true, data: users })
+      } catch (error) {
+        res.status(400).json({ success: false })
+      }
+      break
+    case 'POST':
+      try {
+        const user = await User.create(req.body)
+        res.status(201).json({ success: true, data: user })
+      } catch (error) {
+        res.status(400).json({ success: false })
+      }
+      break
+    default:
+      res.status(400).json({ success: false })
+      break
+  }
+}
